90708c1a6b2bf4f5f5036845badccef357b0b07c
paulnyabaro <paulnyabaro1@gmail.com>
2025-08-10 17:45:33 +0300

    Working with classes

---
+++ class.py
@@ -0,0 +1,14 @@
+class Bike:
+    def __init__(self, color, rim_material):
+        self.color = color
+        self.rim_material = rim_material
+
+    def brake(self):
+        print(self.color + ' Bike braking...')
+
+
+red_bike = Bike("Yellow", "Carbon Fibre")
+green_bike = Bike("Green", "Metal")
+
+print(red_bike.color)
+red_bike.brake()
